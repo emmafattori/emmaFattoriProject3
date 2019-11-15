f4108f1b01032c150d478351dc561dd1576309c7
emmafattori <emmalfattori@gmail.com>
2019-11-14 20:01:51 -0500

    styles set up added, and initial JS

--- script.js
+++ script.js
@@ -1,31 +1,27 @@
 const storyApp = {};
-storyApp.userName = $('input[type=text]').val();
-storyApp.pronoun = $('input[type=radio]:checked').val();
+// This is a function that takes a perameter of the slide number. The slide number corresponds to the html section of the story. The function slides to the slide number that is passed as an argument.
 
-const scrollToNext = function(){
-	$.scrollTo($('#slide1'))
-};
-
-$('form').on('submit', function(e){
-	e.preventDefault();
-// Taking the user inputs, and appending their information into the body of the story.
+const nextPage = function(slideNo){
+	 $('html, body').animate({
+        scrollTop: $(slideNo).offset().top
+    }, 2000);
+}
+	$('form').on('submit', function(e){
+		e.preventDefault();
+	// Taking the user inputs, and appending their information into the body of the story.
+	storyApp.userName = $('input[type=text]').val();
+	storyApp.pronoun = $('input[type=radio').val();
 
 	$('.name').text(storyApp.userName);
 	$('.pronoun').text(storyApp.pronoun);
-// Scrolling to the next section of the story
 
-	$('body').animate(function(){
-	})
-
+	nextPage('#slide1');
+   
+	
 });
 
 
 
-
-
-$('document').ready(function(){
-
-});
 
 
 
